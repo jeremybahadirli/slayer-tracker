5f792025d7bc54b28733b61e627526dad1719767
jeremybahadirli <jeremybahadirli@gmail.com>
2021-12-19 16:23:54 -0600

    Remove Tracked Tasks config entry

--- src/main/java/com/slayertracker/SlayerTrackerConfig.java
+++ src/main/java/com/slayertracker/SlayerTrackerConfig.java
@@ -2,7 +2,6 @@ package com.slayertracker;
 
 import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
-import net.runelite.client.config.ConfigItem;
 
 @ConfigGroup(SlayerTrackerConfig.GROUP_NAME)
 public interface SlayerTrackerConfig extends Config {
@@ -14,5 +13,4 @@ public interface SlayerTrackerConfig extends Config {
     String XP_KEY = "xp_";
     String HA_KEY = "ha_";
     String GE_KEY = "ge_";
-    String TRACKED_TASKS_KEY = "tracked_tasks";
 }
