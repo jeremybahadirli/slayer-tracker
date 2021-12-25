58522bdfd89d496bfd4a3bd169b8201ec2bf0b61
jeremybahadirli <jeremybahadirli@gmail.com>
2021-12-25 10:09:06 -0600

    Add Loot Unit config item

--- src/main/java/com/slayertracker/SlayerTrackerConfig.java
+++ src/main/java/com/slayertracker/SlayerTrackerConfig.java
@@ -2,6 +2,7 @@ package com.slayertracker;
 
 import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
+import net.runelite.client.config.ConfigItem;
 
 @ConfigGroup(SlayerTrackerConfig.GROUP_NAME)
 public interface SlayerTrackerConfig extends Config {
@@ -13,4 +14,18 @@ public interface SlayerTrackerConfig extends Config {
     String XP_KEY = "xp_";
     String HA_KEY = "ha_";
     String GE_KEY = "ge_";
+
+    @ConfigItem(
+            keyName = "lootUnit",
+            name = "Loot Unit",
+            description = "Display loot value as Grand Exchange or High Alchemy price."
+    )
+    default SlayerTrackerLootUnit lootUnit()
+    {
+        return SlayerTrackerLootUnit.GRAND_EXCHANGE;
+    }
+
+    enum SlayerTrackerLootUnit {
+        GRAND_EXCHANGE, HIGH_ALCHEMY
+    }
 }
