b0d97c53eedafc2f25721bc1d6976f61fed0a966
Jeremy Bahadirli <jeremybahadirli@gmail.com>
2026-01-02 15:11:09 -0600

    Assignment refactor

---
+++ src/main/java/com/slayertracker/groups/Match.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright (c) 2022, Jeremy Bahadirli <https://github.com/jeremybahadirli>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.slayertracker.groups;
+
+import java.util.Arrays;
+import java.util.Set;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import net.runelite.api.NPC;
+
+public final class Match
+{
+	private final Predicate<NPC> predicate;
+
+	private Match(Predicate<NPC> predicate)
+	{
+		this.predicate = predicate;
+	}
+
+	static Match byNpcNames(String... npcNames)
+	{
+		Set<String> lowerNames = Arrays.stream(npcNames)
+			.map(String::toLowerCase)
+			.collect(Collectors.toSet());
+
+		return new Match((npc) -> {
+			String name = npc.getName();
+			return name != null && lowerNames.contains(name.toLowerCase());
+		});
+	}
+
+	static Match byCombatLevels(int... combatLevels)
+	{
+		Set<Integer> levelSet = Arrays.stream(combatLevels)
+			.boxed()
+			.collect(Collectors.toSet());
+
+		return new Match((npc) -> levelSet.contains(npc.getCombatLevel()));
+	}
+
+	boolean matches(NPC npc)
+	{
+		return predicate.test(npc);
+	}
+}
---
+++ src/main/java/com/slayertracker/groups/VariantTypeAdapter.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2022, Jeremy Bahadirli <https://github.com/jeremybahadirli>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.slayertracker.groups;
+
+import com.google.gson.TypeAdapter;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonToken;
+import com.google.gson.stream.JsonWriter;
+import java.io.IOException;
+
+public class VariantTypeAdapter extends TypeAdapter<Variant>
+{
+	@Override
+	public void write(JsonWriter out, Variant variant) throws IOException
+	{
+		if (variant == null)
+		{
+			out.nullValue();
+			return;
+		}
+
+		out.value(variant.getId());
+	}
+
+	@Override
+	public Variant read(JsonReader in) throws IOException
+	{
+		if (in.peek() == JsonToken.NULL)
+		{
+			in.nextNull();
+			return null;
+		}
+
+		String id = in.nextString();
+		return Variant.of(id, id);
+	}
+}
