48086f457abec807930ffd158f4703e9ad800af7
Jeremy Bahadirli <jeremybahadirli@gmail.com>
2026-01-19 22:59:51 -0600

    Improve quantity formatting

--- src/main/java/com/slayertracker/views/recordpanels/components/StatsPanel.java
+++ src/main/java/com/slayertracker/views/recordpanels/components/StatsPanel.java
@@ -29,6 +29,7 @@ import com.slayertracker.records.Record;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.GridLayout;
+import javax.annotation.Nullable;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import net.runelite.client.ui.FontManager;
@@ -45,15 +46,39 @@ public class StatsPanel extends JPanel
 		setOpaque(false);
 
 		add(buildStatColumn(new String[][]{
-			{"kc: ", QuantityFormatter.formatNumber(record.getKc())},
-			{"xp: ", QuantityFormatter.formatNumber(record.getXp())},
-			{gpValues.label, gpValues.value}
+			{
+				"kc: ",
+				QuantityFormatter.quantityToRSDecimalStack(record.getKc(), true),
+				QuantityFormatter.formatNumber(record.getKc()) + " kc"
+			},
+			{
+				"xp: ",
+				QuantityFormatter.quantityToRSDecimalStack(record.getXp(), true),
+				QuantityFormatter.formatNumber(record.getXp()) + " xp"
+			},
+			{
+				gpValues.label,
+				gpValues.value,
+				gpValues.valueTooltip
+			}
 		}));
 
 		add(buildStatColumn(new String[][]{
-			{"kc/h: ", QuantityFormatter.formatNumber(Math.round(record.getKc() / record.getHours()))},
-			{"xp/h: ", QuantityFormatter.formatNumber(Math.round(record.getXp() / record.getHours()))},
-			{gpValues.rateLabel, gpValues.rateValue}
+			{
+				"kc/h: ",
+				QuantityFormatter.quantityToRSDecimalStack(Math.round(record.getKc() / record.getHours()), true),
+				QuantityFormatter.formatNumber(Math.round(record.getKc() / record.getHours())) + " kc/h"
+			},
+			{
+				"xp/h: ",
+				QuantityFormatter.quantityToRSDecimalStack(Math.round(record.getXp() / record.getHours()), true),
+				QuantityFormatter.formatNumber(Math.round(record.getXp() / record.getHours())) + " xp/h"
+			},
+			{
+				gpValues.rateLabel,
+				gpValues.rateValue,
+				gpValues.rateValueTooltip
+			}
 		}));
 	}
 
@@ -68,31 +93,35 @@ public class StatsPanel extends JPanel
 
 		for (int rowIndex = 0; rowIndex < rows.length; rowIndex++)
 		{
-			addRow(stats, baseConstraints, rowIndex, rows[rowIndex][0], rows[rowIndex][1]);
+			addRow(stats, baseConstraints, rowIndex, rows[rowIndex][0], rows[rowIndex][1], rows[rowIndex][2]);
 		}
 
 		return stats;
 	}
 
-	private void addRow(JPanel panel, GridBagConstraints template, int row, String labelText, String valueText)
+	private void addRow(JPanel panel, GridBagConstraints template, int row, String labelText, String valueText, String tooltipText)
 	{
 		GridBagConstraints labelConstraints = (GridBagConstraints) template.clone();
 		labelConstraints.weightx = 0;
 		labelConstraints.gridx = 0;
 		labelConstraints.gridy = row;
-		panel.add(createStatLabel(labelText), labelConstraints);
+		panel.add(createStatLabel(labelText, null), labelConstraints);
 
 		GridBagConstraints valueConstraints = (GridBagConstraints) template.clone();
 		valueConstraints.weightx = 1;
 		valueConstraints.gridx = 1;
 		valueConstraints.gridy = row;
-		panel.add(createStatLabel(valueText), valueConstraints);
+		panel.add(createStatLabel(valueText, tooltipText), valueConstraints);
 	}
 
-	private JLabel createStatLabel(String text)
+	private JLabel createStatLabel(String text, @Nullable String tooltipText)
 	{
 		JLabel label = new JLabel(text);
 		label.setFont(FontManager.getRunescapeSmallFont());
+		if (tooltipText != null)
+		{
+			label.setToolTipText(tooltipText);
+		}
 		return label;
 	}
 
@@ -102,22 +131,30 @@ public class StatsPanel extends JPanel
 		private final String value;
 		private final String rateLabel;
 		private final String rateValue;
+		private final String valueTooltip;
+		private final String rateValueTooltip;
 
 		private GpValues(Record record, SlayerTrackerConfig.LootUnit lootUnit)
 		{
 			if (lootUnit.equals(SlayerTrackerConfig.LootUnit.HIGH_ALCHEMY))
 			{
 				label = "ha: ";
-				value = QuantityFormatter.formatNumber(record.getHa());
+				value = QuantityFormatter.quantityToRSDecimalStack(record.getHa(), true);
+				valueTooltip = QuantityFormatter.formatNumber(record.getHa()) + " gp";
+
 				rateLabel = "ha/h: ";
-				rateValue = QuantityFormatter.formatNumber(Math.round(record.getHa() / record.getHours()));
+				rateValue = QuantityFormatter.quantityToRSDecimalStack(Math.round(record.getHa() / record.getHours()), true);
+				rateValueTooltip = QuantityFormatter.formatNumber(Math.round(record.getHa() / record.getHours())) + " gp/h";
 			}
 			else
 			{
 				label = "ge: ";
-				value = QuantityFormatter.formatNumber(record.getGe());
+				value = QuantityFormatter.quantityToRSDecimalStack(record.getGe(), true);
+				valueTooltip = QuantityFormatter.formatNumber(record.getGe()) + " gp";
+
 				rateLabel = "ge/h: ";
-				rateValue = QuantityFormatter.formatNumber(Math.round(record.getGe() / record.getHours()));
+				rateValue = QuantityFormatter.quantityToRSDecimalStack(Math.round(record.getGe() / record.getHours()), true);
+				rateValueTooltip = QuantityFormatter.formatNumber(Math.round(record.getGe() / record.getHours())) + " gp/h";
 			}
 		}
 	}
